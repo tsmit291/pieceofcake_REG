05502ff496495e715de1c4f5451b922ed23ff762
= <tsmit291@icloud.com>
2016-03-05 12:17:06 -0700

    seeds are working

--- express/seeds/create_living.js
+++ express/seeds/create_living.js
@@ -1,12 +1,122 @@
-
 exports.seed = function(knex, Promise) {
   return Promise.join(
     // Deletes ALL existing entries
-    knex('table_name').del(), 
+    knex('living').del(),
 
     // Inserts seed entries
-    knex('table_name').insert({id: 1, colName: 'rowValue'}),
-    knex('table_name').insert({id: 2, colName: 'rowValue2'}),
-    knex('table_name').insert({id: 3, colName: 'rowValue3'})
+    knex('living').insert({
+      item: 'salad bowl',
+      category: 'living',
+      quantity: '1',
+      price range: '$$'
+      }),
+    knex('living').insert({
+      item: 'drinking glasses',
+      category: 'living',
+      quantity: '12',
+      price range: '$$'
+      }),
+    knex('living').insert({
+      item: 'mugs',
+      category: 'living',
+      quantity: '8',
+      price range: '$'
+      }),
+    knex('living').insert({
+      item: 'serving platters',
+      category: 'living',
+      quantity: '2',
+      price range: '$$'
+      }),
+    knex('living').insert({
+      item: 'serving bowls',
+      category: 'living',
+      quantity: '2',
+      price range: '$$'
+      }),
+    knex('living').insert({
+      item: 'serving utensils',
+      category: 'living',
+      quantity: '2',
+      price range: '$$'
+      }),
+    knex('living').insert({
+      item: 'silverware set',
+      category: 'living',
+      quantity: '12',
+      price range: '$$'
+      }),
+    knex('living').insert({
+      item: 'steak knives',
+      category: 'living',
+      quantity: '12',
+      price range: '$$'
+      }),
+    knex('living').insert({
+      item: 'decorative bowls',
+      category: 'living',
+      quantity: '12',
+      price range: '$$'
+      }),
+    knex('living').insert({
+      item: 'throw blanket',
+      category: 'living',
+      quantity: '2',
+      price range: '$$'
+      }),
+    knex('living').insert({
+      item: 'wine glasses',
+      category: 'living',
+      quantity: '6',
+      price range: '$$'
+      }),
+    knex('living').insert({
+      item: 'stemless wine glasses',
+      category: 'living',
+      quantity: '6',
+      price range: '$$'
+      }),
+    knex('living').insert({
+      item: 'everyday glasses',
+      category: 'living',
+      quantity: '12',
+      price range: '$$'
+      }),
+    knex('living').insert({
+      item: 'pint glasses',
+      category: 'living',
+      quantity: '12',
+      price range: '$$'
+      }),
+    knex('living').insert({
+      item: 'wine opener',
+      category: 'living',
+      quantity: '1',
+      price range: '$'
+      }),
+    knex('living').insert({
+      item: 'grilling tools',
+      category: 'living',
+      quantity: '1',
+      price range: '$$'
+      }),
+    knex('living').insert({
+      item: 'chip-and-dip tray',
+      category: 'living',
+      quantity: '1',
+      price range: '$$'
+      }),
+    knex('living').insert({
+      item: 'rolling cooler',
+      category: 'living',
+      quantity: '1',
+      price range: '$$'
+      }),
+    knex('living').insert({
+      item: 'coasters',
+      category: 'living',
+      quantity: '8',
+      price range: '$$'
+      }),
   );
 };
--- express/seeds/create_outdoor.js
+++ express/seeds/create_outdoor.js
@@ -1,12 +1,116 @@
-
 exports.seed = function(knex, Promise) {
   return Promise.join(
     // Deletes ALL existing entries
-    knex('table_name').del(), 
+    knex('kitchen').del(),
 
     // Inserts seed entries
-    knex('table_name').insert({id: 1, colName: 'rowValue'}),
-    knex('table_name').insert({id: 2, colName: 'rowValue2'}),
-    knex('table_name').insert({id: 3, colName: 'rowValue3'})
+    knex('kitchen').insert({
+      item: 'baking sheets',
+      category: 'kitchen',
+      quantity: '2',
+      price range: '$'
+      }),
+    knex('kitchen').insert({
+      item: 'blender',
+      category: 'kitchen',
+      quantity: '1',
+      price range: '$$$'
+      }),
+    knex('kitchen').insert({
+      item: 'coffee maker',
+      category: 'kitchen',
+      quantity: '1',
+      price range: '$$'
+      }),
+    knex('kitchen').insert({
+      item: 'cutting boards',
+      category: 'kitchen',
+      quantity: '2',
+      price range: '$'
+      }),
+    knex('kitchen').insert({
+      item: 'food processor',
+      category: 'kitchen',
+      quantity: '1',
+      price range: '$$'
+      }),
+    knex('kitchen').insert({
+      item: 'knife set',
+      category: 'kitchen',
+      quantity: '1',
+      price range: '$$$'
+      }),
+    knex('kitchen').insert({
+      item: 'oven mitts',
+      category: 'kitchen',
+      quantity: '2',
+      price range: '$'
+      }),
+    knex('kitchen').insert({
+      item: 'round cake pans',
+      category: 'kitchen',
+      quantity: '2',
+      price range: '$$'
+      }),
+    knex('kitchen').insert({
+      item: 'round pie pan',
+      category: 'kitchen',
+      quantity: '1',
+      price range: '$$'
+      }),
+    knex('kitchen').insert({
+      item: 'multipurpose pots',
+      category: 'kitchen',
+      quantity: '2',
+      price range: '$$'
+      }),
+    knex('kitchen').insert({
+      item: 'sauce pan',
+      category: 'kitchen',
+      quantity: '1',
+      price range: '$$'
+      }),
+    knex('kitchen').insert({
+      item: 'skillet',
+      category: 'kitchen',
+      quantity: '1',
+      price range: '$$'
+      }),
+    knex('kitchen').insert({
+      item: 'measuring cups',
+      category: 'kitchen',
+      quantity: '1',
+      price range: '$'
+      }),
+    knex('kitchen').insert({
+      item: 'measuring spoons',
+      category: 'kitchen',
+      quantity: '1',
+      price range: '$'
+      }),
+    knex('kitchen').insert({
+      item: 'measuring bowls',
+      category: 'kitchen',
+      quantity: '1',
+      price range: '$$'
+      }),
+    knex('kitchen').insert({
+      item: 'standing mixer',
+      category: 'kitchen',
+      quantity: '1',
+      price range: '$$$'
+      }),
+    knex('kitchen').insert({
+      item: 'toaster',
+      category: 'kitchen',
+      quantity: '1',
+      price range: '$$'
+      }),
+    knex('kitchen').insert({
+      item: 'wooden spoons',
+      category: 'kitchen',
+      quantity: '2',
+      price range: '$'
+      })
   );
 };
