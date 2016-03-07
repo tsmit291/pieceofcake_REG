ff11982baa83f324a7b33a7719b92d41b0949de7
= <tsmit291@icloud.com>
2016-03-07 08:33:43 -0700

    asdfjkl;

--- ang_app/js/controllers/main_controller.js
+++ ang_app/js/controllers/main_controller.js
@@ -1,11 +1,6 @@
 app.controller('mainController', ['$scope', 'registryService', function($scope, registryService){
-
-  var promise = registryService.getRegistry();
-  $scope.myRegistry = registryService.registry;
-  $scope.add = registryService.add;
-
-  promise.then(function(collection){
-// whatever results are from promise (once it is resolved) this gets set equal to my registry and then I call it.
-    $scope.regCollection = collection.data;
-  });
+  registryService.getItems().then(function(results){
+    console.log(results);
+    $scope.registryCollection = results.data;
+  })
 }]);
--- ang_app/services/registry_service.js
+++ ang_app/services/registry_service.js
@@ -1,14 +1,9 @@
-// get registry on page
-// http lets us link to our json file, $q is to defer our promise
-app.service('registryService', ['$http', '$q', function($http, $q){
-  var deferred = $q.defer();
-  $http.get('https://pieceofcake-reg.herokuapp.com/').then(function (data){
-    deferred.resolve(data);
-  });
-  // says i want to do this later (a promise)
-  // deferred.resolve resolves the data later
-  this.getTea = function (){
-    return deferred.promise;
+app.service("registryService", function($http){
+  var registryService = {};
 
+  registryService.getItems = function(){
+    return $http.get("http://localhost:3000/registries", {method: "jsponp"});
+    // return $http.get("https://pieceofcake.firebaseapp.com/#/", {method: "jsonp"});
   }
-}]);
+  return registryService;
+});
