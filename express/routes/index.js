0503871dcae83aebd9b0e8559e2768532f02886e
= <tsmit291@icloud.com>
2016-03-07 08:39:49 -0700

    asdjfkl;

--- express/routes/index.js
+++ express/routes/index.js
@@ -1,10 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var knex = require('../db/knex.js');
+
+function kitchen(){
+  return knex('kitchen');
+}
+
+function living(){
+  return knex('living');
+}
+
+function outdoor(){
+  return knex('outdoor');
+}
+
+function registry(){
+  return knex('registry');
+}
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  // res.render('index', { title: 'Express' });
-  res.json({ connected: 'for sure'})
+  kitchen().select().then(function (results){
+    res.json(results);
+  });
+
 });
 
 module.exports = router;
